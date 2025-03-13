794b1a9cd46bde458afb9d0c7945a9b3db2919ae
Fernando Alfaro Campos <Fernando.Alfaro_campos@Dell.com>
2025-03-13 14:35:41 -0400

    Add nodemonitor unit tests

--- nfs/nodemonitor_test.go
+++ nfs/nodemonitor_test.go
@@ -20,10 +20,15 @@ import (
 	"context"
 	"net"
 	"testing"
+	"time"
 
+	k8s "github.com/dell/csm-hbnfs/nfs/k8s"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
 func TestGetExports(t *testing.T) {
@@ -163,6 +168,209 @@ func TestGetNnodeExportCounts(t *testing.T) {
 		// Since it is inRecovery, no exports should be found
 		if len(resp) != 0 {
 			t.Error("inRecovery - no exports should be found")
+		}
+	})
+}
+
+func TestPinger(t *testing.T) {
+	t.Run("Fail - No address in node", func(t *testing.T) {
+		s := CsiNfsService{}
+
+		// No need to create a server since no calls are made.
+		req := &v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "myNode",
+			},
+			Status: v1.NodeStatus{
+				Addresses: []v1.NodeAddress{},
+			},
+		}
+
+		// No return value but since there is no addresses, it should cover the error case.
+		go s.pinger(req)
+	})
+
+	t.Run("Success - No address in node", func(t *testing.T) {
+		s := CsiNfsService{}
+
+		ip := "127.0.0.1"
+
+		createMockServer(t, ip, false)
+		req := &v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "myNode",
+			},
+			Status: v1.NodeStatus{
+				Addresses: []v1.NodeAddress{
+					{
+						Type:    v1.NodeInternalIP,
+						Address: ip,
+					},
+				},
+			},
+		}
+
+		// No return value but since there is no addresses, it should cover the error case.
+		go s.pinger(req)
+		time.Sleep(1 * time.Second)
+	})
+
+	t.Run("Success - Recovery attempt", func(t *testing.T) {
+		// Create a new fake clientset
+		clientset := fake.NewSimpleClientset()
+
+		s := CsiNfsService{
+			k8sclient: &k8s.K8sClient{
+				Clientset: clientset,
+			},
+		}
+
+		ip := "127.0.0.1"
+
+		createMockServer(t, ip, true)
+
+		req := &v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "myNode",
+			},
+			Status: v1.NodeStatus{
+				Addresses: []v1.NodeAddress{
+					{
+						Type:    v1.NodeInternalIP,
+						Address: ip,
+					},
+				},
+			},
+		}
+
+		// PingRate = 1 * time.Second
+		setPingRate(1 * time.Second)
+		go s.pinger(req)
+		time.Sleep(4 * time.Second)
+	})
+}
+
+func TestIsControlPlaneNode(t *testing.T) {
+	tests := []struct {
+		name string
+		node *v1.Node
+		want bool
+	}{
+		{
+			name: "node without taints",
+			node: &v1.Node{
+				Spec: v1.NodeSpec{
+					Taints: []v1.Taint{},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "node with taint but not control-plane taint",
+			node: &v1.Node{
+				Spec: v1.NodeSpec{
+					Taints: []v1.Taint{
+						{
+							Key: "node-role.kubernetes.io/other-taint",
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "node with control-plane taint",
+			node: &v1.Node{
+				Spec: v1.NodeSpec{
+					Taints: []v1.Taint{
+						{
+							Key: "node-role.kubernetes.io/control-plane",
+						},
+					},
+				},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isControlPlaneNode(tt.node); got != tt.want {
+				t.Errorf("isControlPlaneNode() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStartNodeMonitor(t *testing.T) {
+	// Test case: startNodeMonitor should not do anything if the node is a control plane node
+	t.Run("ControlPlaneNode", func(t *testing.T) {
+		s := CsiNfsService{}
+		node := &v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "control-plane-node",
+			},
+			Spec: v1.NodeSpec{
+				Taints: []v1.Taint{
+					{
+						Key: "node-role.kubernetes.io/control-plane",
+					},
+				},
+			},
+		}
+
+		s.startNodeMonitor(node)
+	})
+	// Test case: startNodeMonitor should start the pinger goroutine if the node is not a control plane node
+	t.Run("NonControlPlaneNode", func(t *testing.T) {
+		s := CsiNfsService{}
+		node := &v1.Node{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "non-control-plane-node",
+			},
+			Spec: v1.NodeSpec{
+				Taints: []v1.Taint{
+					{
+						Key: "NonControlPlaneNodeTaint",
+					},
+				},
+			},
+		}
+
+		s.startNodeMonitor(node)
+	})
+}
+
+func TestGetNodeStatus(t *testing.T) {
+	t.Run("Success: Valid IPs", func(t *testing.T) {
+		s := &CsiNfsService{}
+		nodeStatus := &NodeStatus{
+			nodeName: "myNode",
+			nodeIp:   "127.0.0.1",
+		}
+
+		nodeIpToStatus["127.0.0.1"] = nodeStatus
+
+		status := s.GetNodeStatus("127.0.0.1")
+
+		if status != nodeStatus {
+			t.Errorf("Expected same status, got %v", status)
+		}
+	})
+
+	t.Run("Success: IP not found", func(t *testing.T) {
+		s := &CsiNfsService{}
+		nodeStatus := &NodeStatus{
+			nodeName: "myNode",
+			nodeIp:   "127.0.0.1",
+		}
+
+		nodeIpToStatus[nodeStatus.nodeIp] = nodeStatus
+
+		// Look for a different node IP
+		status := s.GetNodeStatus("127.0.0.2")
+
+		if status != nil {
+			t.Errorf("Expected nil status, got %v", status)
 		}
 	})
 }
