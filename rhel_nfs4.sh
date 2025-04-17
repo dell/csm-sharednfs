7d408ea16c51ba9ca7d9602f5c8a19c40bbb39b5
Lau, Luke <luke.lau1@dell.com>
2025-04-17 16:12:16 -0400

    fixing syntax issues and typos in rhel_nfs4.sh script

--- rhel_nfs4.sh
+++ rhel_nfs4.sh
@@ -17,14 +17,14 @@ fi
 
 
 echo "copying nfs.conf"
-copy cp rhel_nfs.conf /etc/nfs.conf
+echo cp rhel_nfs.conf /etc/nfs.conf
 cp rhel_nfs.conf /etc/nfs.conf
 
 echo "disabling v3 services"
 echo systemctl mask --now rpc-statd.service rpcbind.service rpcbind.socket
 systemctl mask --now rpc-statd.service rpcbind.service rpcbind.socket
 
-"configuring nfs-mount.service.d"
+echo "configuring nfs-mount.service.d"
 echo mkdir /etc/systemd/nfs-mount.service.d
 mkdir /etc/systemd/nfs-mount.service.d
 cat >/etc/systemd/nfs-mount.service.d/v4only.conf <<END
@@ -40,7 +40,7 @@ systemctl restart nfs-mountd
 
 echo "should see nfsd processes: "
 ps axl | grep nfsd
-echo "... done
+echo "... done"
 
 mkdir /nfs
 mkdir /nfs/exports
@@ -55,13 +55,12 @@ echo "setting test export"
 echo mkdir /nfs/exports
 mkdir /nfs/exports
 echo chgrp users /nfs/exports
-echo chgrp users /nfs/exports
 chgrp users /nfs/exports
 echo chmod 02770 /nfs/exports
 chmod 02770 /nfs/exports
 echo mkdir /nfs/exports/hello
 mkdir /nfs/exports/hello
-echochgrp users /nfs/exports/hello
+echo chgrp users /nfs/exports/hello
 chgrp users /nfs/exports/hello
 echo chmod 02777 /nfs/exports/hello
 chmod 02777 /nfs/exports/hello
@@ -70,6 +69,6 @@ echo "hello world" > /nfs/exports/hello/world
 echo "updating /etc/exports"
 echo "/nfs/exports/hello/  $ip/24(rw)" >> /etc/exports
 cat /etc/exports
-systemctl restart nfs.mountd
+systemctl restart nfs-mountd
 
-echo "check that you can mount /nfs/exports/hello with\nmkdir hello && mount -t nfs4 $ip:/exports/hello /hello"
+echo -e "check that you can mount /nfs/exports/hello with\n\tmkdir ./hello && mount -t nfs4 $ip:/nfs/exports/hello ./hello"
